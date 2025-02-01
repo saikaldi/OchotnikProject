ef96126a6a44beec8523b68802572711b1c7a987
Ernst <ernistturgunbaev01@gmail.com>
2025-02-01 20:19:43 +0500

    ordering-app

--- config/settings.py
+++ config/settings.py
@@ -124,10 +124,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "ernistturgunbaev01@gmail.com"
-EMAIL_HOST_PASSWORD = "tzim zcay jcpy dvuf"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
